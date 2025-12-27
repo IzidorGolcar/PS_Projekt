b9b89e825a51e318128a6a8f5baba9ff3f83229b
Izidor Golčar <izidor.golcar@gmail.com>
2025-12-27 15:18:21 +0100

    Remove generated protobuf files for control and datalink APIs

--- proto/generate.go
+++
@@ -1,4 +0,0 @@
-package proto
-
-//go:generate protoc --proto_path=. --go_out=paths=import:.. --go-grpc_out=paths=import:.. razpravljalnica.proto datalink.proto control.proto raft.proto
-//go:generate sh -c "mv ../seminarska/proto/razpravljalnica/* razpravljalnica/ && mv ../seminarska/proto/datalink/* datalink/ && mv ../seminarska/proto/controllink/* controllink/ && mv ../seminarska/proto/raft/* raft/ && rm -rf ../seminarska/"
